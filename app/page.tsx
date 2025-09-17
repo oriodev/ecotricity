6d8b292a49a91555a0b517ff65000ede2c39be1a
oriodev <oriodev@duck.com>
2025-09-17 16:39:08 +0100

    [api-call]: Fetches resources from NESO's public API, returns a modified version of each resource, and renders them on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import { getResources } from "@/api/resources.api";
+
+export default async function Home() {
+
+  // Fetch resources
+  const resources = await getResources();
+
   return (
     <div>
-      <p>ecotricity</p>
+      <p>Ecotricity</p>
+
+      {/* Renders resources */}
+      <div>
+        {
+          resources.map(resource => (
+            <div key={resource.name}>
+              <h3>{resource.name}</h3>
+              <p>{resource.description}</p>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
---
+++ types.ts
@@ -0,0 +1,29 @@
+export interface Resource {
+  name: string;
+  description: string;
+  url: string;
+}
+
+export interface FullResource {
+    mimetype: string | null;
+    cache_url: string | null;
+    name: string;
+    hash: string;
+    description: string;
+    metadata_modified: string;
+    cache_last_updated: string | null;
+    url: string;
+    datastore_active: boolean;
+    format: string;
+    package_id: string;
+    created: string;
+    state: string;
+    mimetype_inner: string | null;
+    last_modified: string;
+    position: number;
+    revision_id: string;
+    url_type: string;
+    id: string;
+    resource_type: string | null;
+    size: number | null;
+}
