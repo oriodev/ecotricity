52b505ce73258fdce63b3a97adc8d1d978cd234b
oriodev <oriodev@duck.com>
2025-09-25 13:40:49 +0100

    [add-greycliff-font]: add greycliff font to component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from 'next/font/local'
+
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const greycliff = localFont({ src: '../public/fonts/greycliffcf-regular.otf'})
 
 export const metadata: Metadata = {
   title: "Ecotricity",
@@ -25,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${greycliff.className} antialiased`}
       >
         {children}
       </body>
